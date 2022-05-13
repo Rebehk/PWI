4b29356ddd1f0ba93da50d471b5512b666706074
Rebehk Jordão Silva <201922010012@ad.ifpb.edu.br>
2022-05-13 14:21:43 -0300

    update

--- simplerouting.js
+++ simplerouting.js
@@ -1,23 +1,51 @@
 const http = require('http')
 const port = process.env.PORT || 3000
-const server = http.createServer((req,res) =>{
-    //normaliza a url removendo a querystring e barra final
-    //opcional usando letras minusculas
-    const path =req.url.replace(/\?(?;\?.*)?$/, '').toLowerCase()
+
+const home = `
+    <h2>Oiee, seja bem-vinde </h2>
+    <h3> 
+        Meu nome é Rebehk, sou estudante de Análise e Desenvolvimento de sistemas no IFPB, <br>
+        sou vegetariana, amo animais, series e filmes. 
+
+    </h3>
+    
+`;
+
+const curriculo =`
+    <h4>Curriculo</h4>
+  
+    <ul>
+        <h5>Formação e experiencias academicas</h5>
+        <li>Cursando ADS no IFPB, Fui monitora de algoritmos e lógica de programação.<br>
+         Estou partipando de um projeto de extensão no qual estou desenvolvendo aplicativo com React Native para criar um aambiente gamificado Crealit como ferramenta de incentivo à leitura, de livros clássicos da literatura brasileira  
+        </li>
+        <h5>Experincias profissionais</h5>
+        <li>Em busca do primiero emprego na area de TI</li>
+        
+    </ul>
+
+`;
+
+
+
+
+const server = http.createServer((req,res) => {
+    // normaliza a url removendo a querystring e a barra final
+    // opcional e usando letras minúsculas
+    const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
     switch(path) {
         case '':
-            res.writeHead(200, {'Content-Type': 'text/plain'})
-            res.end('Homepage')
+            res.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8' })
+            res.end(home)
             break
-        case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'})
-            res.end('About')
+        case '/curriculo':
+            res.writeHead(200, { 'Content-Type': 'text/html; charset= utf-8' })
+            res.end(curriculo)
             break
         default:
-            res.writeHead(404, {'Content-Type': 'text/plain'})
+            res.writeHead(404, { 'Content-Type': 'text/plain; charset= utf-8' })
             res.end('Not Found')
             break
     }
-});
-
-server.listen(port, ()=> console.log(`server started on port ${port};` + `press Ctrl-C to terrinate....`))
+})
+server.listen(port, () => console.log(`server started on port ${port}; ` + 'press Ctrl-C to terminate....'))
